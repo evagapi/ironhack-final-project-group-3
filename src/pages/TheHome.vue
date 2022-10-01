33f0fcdbe3112bd2fe6e70192d9088e54418231e
Alejocg <107270031+Alejocg@users.noreply.github.com>
2022-10-01 23:37:00 +0200

    Add LandingFeatures component (#32)

--- src/pages/TheHome.vue
+++ src/pages/TheHome.vue
@@ -1,7 +1,11 @@
 <template>
   <div><LandingHero></LandingHero></div>
+  <div><LandingFeatures></LandingFeatures></div>
+  <div><LandingScreenshot></LandingScreenshot></div>
 </template>
 
 <script setup>
 import LandingHero from "../components/LandingHero.vue";
+import LandingFeatures from "../components/LandingFeatures.vue";
+import LandingScreenshot from "../components/LandingScreenshot.vue";
 </script>

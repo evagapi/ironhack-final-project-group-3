19f64633a7cf48523726c9f768d9e8146939fdb1
Alejo <alejoceretogomez@gmail.com>
2022-10-01 10:03:22 +0200

    Script removed + hidden password in form

--- src/components/LoginRegister.vue
+++ src/components/LoginRegister.vue
@@ -141,7 +141,7 @@
               >
               <input
                 id="confirm-password"
-                type="confirm-password"
+                type="password"
                 name="confirm-password"
                 placeholder="Confirm new password"
                 class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -191,5 +191,3 @@
 </template>
 
 <script></script>
-
-<style scoped></style>

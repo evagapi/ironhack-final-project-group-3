b9b64591a57c80cb0e9f1a80143464395cb07bbb
Hyunji Yang <alyang41@gmail.com>
2022-10-03 00:47:15 +0200

    register naive ui trial

--- src/components/LoginRegister.vue
+++ src/components/LoginRegister.vue
@@ -1,3 +1,4 @@
+<!-- eslint-disable max-lines -->
 <template>
   <section
     class="bg-gray-50 dark:bg-gray-900 flex min-h-full min-w-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8 flex-row"
@@ -94,60 +95,75 @@
             </form>
           </div>
         </div>
+
+        <!-- Register -->
         <div
           class="flex p-6 space-y-4 md:space-y-6 sm:p-8 flex-auto flex-wrap justify-center justify-items-center content-center"
         >
-          <form class="space-y-4 md:space-y-6" action="#">
+          <n-form
+            ref="formRef"
+            inline
+            :label-width="80"
+            :model="formValue"
+            :rules="rules"
+            :size="medium"
+            class="space-y-4 md:space-y-6"
+            action="#"
+          >
             <h1
               class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white"
             >
               Create new account
             </h1>
-            <div>
-              <label
-                for="email"
-                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >Your email</label
+
+            <n-form-item
+              path="user.email"
+              label="Your email"
+              :rule="rule"
+              class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
               >
-              <input
+              <n-input
                 id="email"
-                type="email"
-                name="email"
-                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                v-model:value="model.user.email"
                 placeholder="name@email.com"
-                required=""
+                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                @keydown.enter.prevent
               />
-            </div>
-            <div>
-              <label
-                for="password"
-                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >Password</label
-              >
-              <input
+              />
+            </n-form-item>
+            <n-form-item
+              path="password"
+              label="Password"
+              class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              <n-input
                 id="password"
+                v-model:value="model.password"
                 type="password"
-                name="password"
                 placeholder="Create password"
                 class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                required=""
+                @input="handlePasswordInput"
+                @keydown.enter.prevent
               />
-            </div>
-            <div>
-              <label
-                for="confirm-password"
-                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >Confirm password</label
-              >
-              <input
+            </n-form-item>
+            <n-form-item
+              ref="rPasswordFormItemRef"
+              first
+              path="reenteredPassword"
+              label="Confirm password"
+              class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              <n-input
                 id="confirm-password"
+                v-model:value="model.reenteredPassword"
+                :disabled="!model.password"
                 type="password"
-                name="confirm-password"
                 placeholder="Confirm new password"
                 class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                required=""
+                @keydown.enter.prevent
               />
-            </div>
+            </n-form-item>
             <div class="flex items-start">
               <div class="flex items-center h-5">
                 <input
@@ -155,7 +171,6 @@
                   aria-describedby="terms"
                   type="checkbox"
                   class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                  required=""
                 />
               </div>
               <div class="ml-3 text-sm">
@@ -183,11 +198,13 @@
                 </div>
               </div>
             </div>
-          </form>
+          </n-form>
         </div>
       </div>
     </div>
   </section>
 </template>
 
-<script></script>
+<script setup>
+import { NForm, NFormItem, NInput } from "naive-ui";
+</script>

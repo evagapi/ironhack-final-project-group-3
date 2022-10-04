884ac44299face9121a59acafe445d42d871ff0a
Alejocg <107270031+Alejocg@users.noreply.github.com>
2022-10-04 18:26:02 +0200

    feat: add footer (#36)

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,11 @@
   <n-message-provider>
     <router-view />
   </n-message-provider>
+  <MainFooter />
 </template>
 
 <script setup>
 import MainNavbar from "./components/MainNavbar.vue";
 import { NMessageProvider } from "naive-ui";
+import MainFooter from "./components/MainFooter.vue";
 </script>

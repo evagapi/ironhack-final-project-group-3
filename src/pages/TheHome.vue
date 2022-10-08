33e15113e3036a5e5a090706f55bb08da6205cbd
Eva Garcia Pinos <eva.gapi@gmail.com>
2022-10-08 16:53:14 +0200

    feat: redirect to dashboard if user is logged in

--- src/pages/TheHome.vue
+++ src/pages/TheHome.vue
@@ -6,8 +6,21 @@
 </template>
 
 <script setup>
+import { onMounted } from "vue";
+
 import LandingHero from "../components/LandingHero.vue";
 import LandingFeatures from "../components/LandingFeatures.vue";
 import LandingScreenshot from "../components/LandingScreenshot.vue";
 import AboutUs from "../components/AboutUs.vue";
+
+import { useUserStore } from "../stores/UserStore";
+import router from "../router";
+
+const { auth } = useUserStore();
+
+onMounted(() => {
+  if (auth.user) {
+    router.push({ name: "dashboard" });
+  }
+});
 </script>

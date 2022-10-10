e7498338a9d8ad0156ceaa8464c4ec32af41e6f2
Eva Garcia Pinos <eva.gapi@gmail.com>
2022-10-10 21:12:39 +0200

    fix: fix style conflict between naive and tailwind

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import router from "./router";
 import "./style.css";
 import App from "./App.vue";
 
+const meta = document.createElement("meta");
+meta.name = "naive-ui-style";
+document.head.appendChild(meta);
+
 const pinia = createPinia();
 const app = createApp(App);
 
